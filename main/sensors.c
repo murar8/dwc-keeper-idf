d187d19e738b4a6771200e2a2d70f0ae07eeb4c2
Lorenzo Murarotto <murar8@proton.me>
2025-06-27 16:26:30 +0200

    Refactor sensor configuration and data handling: Update sensor ADC channel definitions and enhance buffer management for sensor data. Introduce inline functions for buffer operations and improve logging in the on_adc_continuous_data_ready function. Adjust sensor initialization to utilize new configurations and ensure type safety.

--- main/sensors.c
+++ main/sensors.c
@@ -2,54 +2,145 @@
 
 #include <esp_adc/adc_continuous.h>
 #include <esp_log.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/ringbuf.h>
 #include <soc/adc_channel.h>
 #include <soc/soc_caps.h>
 
-#define CONFIG_SENSORS_PH_ADC_CHANNEL 0
-#define CONFIG_SENSORS_PH_TEMPCO_ADC_CHANNEL 0
-#define CONFIG_SENSORS_EC_ADC_CHANNEL ADC1_GPIO35_CHANNEL
+#define CONFIG_SENSOR_BUF_SIZE 8
 
-#define CONFIG_SENSORS_PH_ADC_ATTEN ADC_ATTEN_DB_12
-#define CONFIG_SENSORS_PH_TEMPCO_ADC_ATTEN ADC_ATTEN_DB_12
-#define CONFIG_SENSORS_EC_ADC_ATTEN ADC_ATTEN_DB_12
+#define SENSOR_EC_ADC_ATTEN ADC_ATTEN_DB_12
+#define SENSOR_PH_ADC_ATTEN ADC_ATTEN_DB_12
+#define SENSOR_PH_TEMPCO_ADC_ATTEN ADC_ATTEN_DB_12
 
-#define SENSORS_ADC_UNIT ADC_UNIT_1
-#define SENSORS_ADC_CONV_MODE ADC_CONV_SINGLE_UNIT_1
-#define SENSORS_ADC_BIT_WIDTH ADC_BITWIDTH_12
-#define SENSORS_ADC_CONV_FRAME_SIZE SOC_ADC_DIGI_DATA_BYTES_PER_CONV * 1024
-#define SENSORS_ADC_MAX_STORE_BUF_SIZE SENSORS_ADC_CONV_FRAME_SIZE * 2
-#define SENSORS_ADC_SAMPLE_FREQ_HZ SOC_ADC_SAMPLE_FREQ_THRES_LOW
-#define SENSORS_ADC_FORMAT ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define SENSOR_ADC_UNIT ADC_UNIT_1
+#define SENSOR_ADC_CONV_MODE ADC_CONV_SINGLE_UNIT_1
+#define SENSOR_ADC_BIT_WIDTH ADC_BITWIDTH_12
+#define SENSOR_ADC_SAMPLE_FREQ_HZ SOC_ADC_SAMPLE_FREQ_THRES_LOW
+#define SENSOR_ADC_FORMAT ADC_DIGI_OUTPUT_FORMAT_TYPE1
+
+#define SENSOR_ADC_SAMPLES_PER_FRAME 4096
+#define SENSOR_ADC_CONV_FRAME_SIZE (SOC_ADC_DIGI_DATA_BYTES_PER_CONV * SENSOR_ADC_SAMPLES_PER_FRAME)
+#define SENSOR_ADC_MAX_STORE_BUF_SIZE (SENSOR_ADC_CONV_FRAME_SIZE)
+
+#define SENSOR_ADC_MAX_VALUE ((1 << SENSOR_ADC_BIT_WIDTH) - 1)
 
 typedef struct
 {
-    adc_channel_t adc_channel;
+    sensor_adc_channel_t adc_channel;
     adc_atten_t adc_atten;
 } sensor_config_t;
 
-static const char *TAG = "sensors";
-
 static const sensor_config_t sensor_configs[] = {
     {
-        .adc_channel = CONFIG_SENSORS_EC_ADC_CHANNEL,
-        .adc_atten = CONFIG_SENSORS_EC_ADC_ATTEN,
+        .adc_channel = SENSOR_EC_ADC_CHANNEL,
+        .adc_atten = SENSOR_EC_ADC_ATTEN,
     },
-    // {
-    //     .adc_channel = CONFIG_SENSORS_PH_ADC_CHANNEL,
-    //     .adc_atten = CONFIG_SENSORS_PH_ADC_ATTEN,
-    // },
-    // {
-    //     .adc_channel = CONFIG_SENSORS_PH_TEMPCO_ADC_CHANNEL,
-    //     .adc_atten = CONFIG_SENSORS_PH_TEMPCO_ADC_ATTEN,
-    // },
+    {
+        .adc_channel = SENSOR_PH_ADC_CHANNEL,
+        .adc_atten = SENSOR_PH_ADC_ATTEN,
+    },
+    {
+        .adc_channel = SENSOR_PH_TEMPCO_ADC_CHANNEL,
+        .adc_atten = SENSOR_PH_TEMPCO_ADC_ATTEN,
+    },
 };
+
+#define SENSOR_COUNT (sizeof(sensor_configs) / sizeof(sensor_configs[0]))
+
+static const char *TAG = "sensors";
+
+static size_t buffer_start_indexes[SENSOR_COUNT] = {[0 ... SENSOR_COUNT - 1] = 0};
+static size_t buffer_end_indexes[SENSOR_COUNT] = {[0 ... SENSOR_COUNT - 1] = 0};
+static uint16_t buffers[SENSOR_COUNT][CONFIG_SENSOR_BUF_SIZE] = {
+    [0 ... SENSOR_COUNT - 1] = {[0 ... CONFIG_SENSOR_BUF_SIZE - 1] = 0}};
+
+static inline size_t buffer_length(size_t sensor_idx)
+{
+    return (buffer_end_indexes[sensor_idx] - buffer_start_indexes[sensor_idx] + CONFIG_SENSOR_BUF_SIZE) %
+           CONFIG_SENSOR_BUF_SIZE;
+}
+
+static void buffer_put(size_t sensor_idx, uint16_t item)
+{
+    buffers[sensor_idx][buffer_end_indexes[sensor_idx]] = item;
+    buffer_end_indexes[sensor_idx] = (buffer_end_indexes[sensor_idx] + 1) % CONFIG_SENSOR_BUF_SIZE;
+    if (buffer_end_indexes[sensor_idx] == buffer_start_indexes[sensor_idx])
+        buffer_start_indexes[sensor_idx] = (buffer_start_indexes[sensor_idx] + 1) % CONFIG_SENSOR_BUF_SIZE;
+}
+
+static uint16_t buffer_get(size_t sensor_idx, size_t offset)
+{
+    size_t length = buffer_length(sensor_idx);
+    if (length == 0 || offset >= length)
+        return 0;
+    size_t idx = (buffer_start_indexes[sensor_idx] + offset) % length;
+    return buffers[sensor_idx][idx];
+}
+
+static ssize_t sensor_get_idx(sensor_adc_channel_t channel)
+{
+    for (size_t i = 0;; i++)
+    {
+        if (i == SENSOR_COUNT)
+            return -1;
+        else if (sensor_configs[i].adc_channel == channel)
+            return i;
+    }
+}
+
+int16_t sensor_get_value(sensor_adc_channel_t channel)
+{
+    ssize_t sensor_idx = sensor_get_idx(channel);
+    if (sensor_idx == -1)
+        return -1;
+    uint32_t sum = 0;
+    size_t length = buffer_length(sensor_idx);
+    for (size_t i = 0; i < length; i++)
+    {
+        sum += buffer_get(sensor_idx, i);
+    }
+    return (int16_t)(sum / length);
+}
 
 static bool on_adc_continuous_data_ready(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata,
                                          void *user_data)
 {
-    adc_digi_output_data_t *p = (adc_digi_output_data_t *)&edata->conv_frame_buffer[0];
-    ESP_DRAM_LOGI(TAG, "sensors_init: channel: %d, data: %d", p->type1.channel, p->type1.data);
-    return false;
+    uint32_t num_bytes = edata->size;
+    uint32_t num_samples = num_bytes / sizeof(adc_digi_output_data_t);
+    adc_digi_output_data_t *p = (adc_digi_output_data_t *)edata->conv_frame_buffer;
+
+    uint32_t totals[SENSOR_COUNT] = {[0 ... SENSOR_COUNT - 1] = 0};
+    uint32_t num_samples_per_sensor[SENSOR_COUNT] = {[0 ... SENSOR_COUNT - 1] = 0};
+    for (int data_idx = 0; data_idx < num_samples; data_idx++)
+    {
+        for (size_t sensor_idx = 0; sensor_idx < SENSOR_COUNT; sensor_idx++)
+        {
+            if (p[data_idx].type1.channel == sensor_configs[sensor_idx].adc_channel)
+            {
+                uint16_t raw_value = p[data_idx].type1.data;
+                totals[sensor_idx] += raw_value;
+                num_samples_per_sensor[sensor_idx]++;
+            }
+        }
+    }
+
+    for (size_t sensor_idx = 0; sensor_idx < SENSOR_COUNT; sensor_idx++)
+    {
+        if (num_samples_per_sensor[sensor_idx] != 0)
+            buffer_put(sensor_idx, totals[sensor_idx] / num_samples_per_sensor[sensor_idx]);
+    }
+
+    // Must be in DRAM!!!
+    uint16_t value = buffer_get(0, 0);
+    ESP_DRAM_LOGI(
+        TAG,
+        "on_adc_continuous_data_ready: %lu | AVG %d | RAW %d | START %d | END %d | SAMPLES %d | TOTALS %d | VALUE %d",
+        pdTICKS_TO_MS(xTaskGetTickCountFromISR()), sensor_get_value(SENSOR_EC_ADC_CHANNEL),
+        totals[0] / num_samples_per_sensor[0], buffer_start_indexes[0], buffer_end_indexes[0],
+        num_samples_per_sensor[0], totals[0], value);
+
+    return true;
 }
 
 void sensors_init()
@@ -57,27 +148,26 @@ void sensors_init()
     ESP_LOGI(TAG, "sensors_init: starting");
 
     adc_continuous_handle_cfg_t adc_config = {
-        .max_store_buf_size = SENSORS_ADC_MAX_STORE_BUF_SIZE,
-        .conv_frame_size = SENSORS_ADC_CONV_FRAME_SIZE,
+        .max_store_buf_size = SENSOR_ADC_MAX_STORE_BUF_SIZE,
+        .conv_frame_size = SENSOR_ADC_CONV_FRAME_SIZE,
     };
     adc_continuous_handle_t handle = NULL;
     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
 
-    static const int config_count = sizeof(sensor_configs) / sizeof(sensor_configs[0]);
-    adc_digi_pattern_config_t adc_pattern[config_count];
-    for (int i = 0; i < config_count; i++)
+    adc_digi_pattern_config_t adc_pattern[SENSOR_COUNT];
+    for (int i = 0; i < SENSOR_COUNT; i++)
     {
         ESP_LOGI(TAG, "sensors_init: configuring sensor %d", i);
-        adc_pattern[i].unit = SENSORS_ADC_UNIT;
-        adc_pattern[i].bit_width = SENSORS_ADC_BIT_WIDTH;
+        adc_pattern[i].unit = SENSOR_ADC_UNIT;
+        adc_pattern[i].bit_width = SENSOR_ADC_BIT_WIDTH;
         adc_pattern[i].atten = sensor_configs[i].adc_atten;
         adc_pattern[i].channel = sensor_configs[i].adc_channel;
     }
     adc_continuous_config_t dig_cfg = {
-        .sample_freq_hz = SENSORS_ADC_SAMPLE_FREQ_HZ,
-        .conv_mode = SENSORS_ADC_CONV_MODE,
-        .format = SENSORS_ADC_FORMAT,
-        .pattern_num = config_count,
+        .sample_freq_hz = SENSOR_ADC_SAMPLE_FREQ_HZ,
+        .conv_mode = SENSOR_ADC_CONV_MODE,
+        .format = SENSOR_ADC_FORMAT,
+        .pattern_num = SENSOR_COUNT,
         .adc_pattern = adc_pattern,
     };
     ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
--- main/sensors.h
+++ main/sensors.h
@@ -1,3 +1,19 @@
 #pragma once
 
+#include <soc/adc_channel.h>
+#include <stdint.h>
+
+#define CONFIG_SENSOR_EC_ADC_CHANNEL ADC1_GPIO35_CHANNEL
+#define CONFIG_SENSOR_PH_ADC_CHANNEL ADC1_GPIO34_CHANNEL
+#define CONFIG_SENSOR_PH_TEMPCO_ADC_CHANNEL ADC1_GPIO36_CHANNEL
+
+typedef enum
+{
+    SENSOR_EC_ADC_CHANNEL = CONFIG_SENSOR_EC_ADC_CHANNEL,
+    SENSOR_PH_ADC_CHANNEL = CONFIG_SENSOR_PH_ADC_CHANNEL,
+    SENSOR_PH_TEMPCO_ADC_CHANNEL = CONFIG_SENSOR_PH_TEMPCO_ADC_CHANNEL,
+} sensor_adc_channel_t;
+
 void sensors_init();
+
+int16_t sensor_get_value(sensor_adc_channel_t channel);
